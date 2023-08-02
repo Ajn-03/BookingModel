f8ab1188ed0b0be95a2d8136a3e5b7a17ecc1f84
Ajn-03 <124113319+Ajn-03@users.noreply.github.com>
2023-08-02 14:51:34 +0530

    Add files via upload

---
+++ clovia/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Login, Bookings
+from django.core.exceptions import ValidationError
+
+def validate_username(value):
+    if not (value.endswith("@clovia.com") or value.endswith("@purplepanda.in")):
+        raise ValidationError("Enter Organization Email Id")
+        
+class LoginForm(forms.ModelForm):
+    username=forms.EmailField(validators=[validate_username])
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = Login
+        fields = ('username', 'password')
+        
+class BookingForm(forms.ModelForm):
+    #booking_time = forms.TimeField(widget=forms.TimeInput)
+    class Meta:
+        model=Bookings
+        fields=('booking_date','booking_time')
+        widgets = {
+            'booking_date': forms.DateInput(attrs={'type': 'date'}),            
+            'booking_time': forms.TimeInput(attrs={'type': 'time'}),
+            'end_time': forms.TimeInput(attrs={'type': 'time'}),
+        }
+        
+
